071d116fb39581539ea861699a7c139eaaa74175
aj333git <amit.doeacc.joshi@gmail.com>
2023-05-24 18:32:17 +0530

    initial  commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.Use(async (context, next) =>{
+    await context.Response.WriteAsync("^^^^^^");
+    await next();
+    await context.Response.WriteAsync("^^^^^^");
+});
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("###### ");
+    await next();
+    await context.Response.WriteAsync(" ######");
+});
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Hello AspNet Core!");
+});
+
+app.Run();
+
+
